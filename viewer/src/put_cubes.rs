6980d4dfb0ea7db5fcc69b08da32fc69a58252c3
沢谷 邦夫 <devel@fathens.org>
2022-01-10 21:47:15 +0900

    Make a hole
    and fix range of color value

--- viewer/src/put_cubes.rs
+++ viewer/src/put_cubes.rs
@@ -1,6 +1,7 @@
 use crate::resources::CubesResource;
 use alignment::model::*;
 use bevy::prelude::*;
+use shuffle::rand_hole;
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq, Component)]
 pub struct CubeHome(Pos3D);
@@ -22,79 +23,81 @@ pub fn generate_cubes(
         resource.cube_size * v
     };
 
-    generate_surfaces(resource.spaces)
+    let hole = rand_hole(resource.spaces);
+    let cubes = generate_surfaces(resource.spaces)
         .into_iter()
-        .for_each(|home| {
-            let center = Vec3::new(
-                calc_pos(home.x(), resource.spaces.x()),
-                calc_pos(home.y(), resource.spaces.y()),
-                calc_pos(home.z(), resource.spaces.z()),
-            );
+        .filter(|home| *home != hole);
 
-            /* TODO Add a transparent cube body
-            commands
-                .spawn()
-                .insert(CubeHome(home))
-                .insert(CubePos(home))
-                .insert_bundle(PbrBundle {
-                    mesh: meshes.add(Mesh::from(shape::Cube {
-                        size: resource.cube_size * 0.9,
-                    })),
-                    material: materials.add(Color::rgba(0.0, 150.0, 200.0, 0.1).into()),
-                    transform: Transform::from_translation(center),
-                    ..Default::default()
-                });
-            */
+    cubes.into_iter().for_each(|home| {
+        let center = Vec3::new(
+            calc_pos(home.x(), resource.spaces.x()),
+            calc_pos(home.y(), resource.spaces.y()),
+            calc_pos(home.z(), resource.spaces.z()),
+        );
 
-            let face_size = resource.cube_size * 0.94;
-            let face_half = 1.01 * face_size / 2.0;
+        /* TODO Add a transparent cube body */
+        commands
+            .spawn()
+            .insert(CubeHome(home))
+            .insert(CubePos(home))
+            .insert_bundle(PbrBundle {
+                mesh: meshes.add(Mesh::from(shape::Cube {
+                    size: resource.cube_size * 0.9,
+                })),
+                material: materials.add(Color::rgb(0.0, 0.9, 0.7).into()),
+                transform: Transform::from_translation(center),
+                ..Default::default()
+            });
 
-            home.get_faces(resource.spaces)
-                .into_iter()
-                .for_each(|direction| {
-                    let tr = match direction {
-                        Direction3D::XNega => Transform {
-                            translation: center + Vec3::new(-face_half, 0.0, 0.0),
-                            rotation: Quat::from_rotation_z(90_f32.to_radians()),
-                            ..Default::default()
-                        },
-                        Direction3D::XPosi => Transform {
-                            translation: center + Vec3::new(face_half, 0.0, 0.0),
-                            rotation: Quat::from_rotation_z(-90_f32.to_radians()),
-                            ..Default::default()
-                        },
-                        Direction3D::YNega => Transform {
-                            translation: center + Vec3::new(0.0, -face_half, 0.0),
-                            rotation: Quat::from_rotation_z(180_f32.to_radians()),
-                            ..Default::default()
-                        },
-                        Direction3D::YPosi => Transform {
-                            translation: center + Vec3::new(0.0, face_half, 0.0),
-                            ..Default::default()
-                        },
-                        Direction3D::ZNega => Transform {
-                            translation: center + Vec3::new(0.0, 0.0, -face_half),
-                            rotation: Quat::from_rotation_x(-90_f32.to_radians()),
-                            ..Default::default()
-                        },
-                        Direction3D::ZPosi => Transform {
-                            translation: center + Vec3::new(0.0, 0.0, face_half),
-                            rotation: Quat::from_rotation_x(90_f32.to_radians()),
-                            ..Default::default()
-                        },
-                    };
+        let face_size = resource.cube_size * 0.94;
+        let face_half = 1.01 * face_size / 2.0;
 
-                    commands
-                        .spawn()
-                        .insert(CubeHome(home))
-                        .insert(CubePos(home))
-                        .insert(CubeFace(direction))
-                        .insert_bundle(PbrBundle {
-                            mesh: meshes.add(Mesh::from(shape::Plane { size: face_size })),
-                            material: materials.add(Color::rgb(200.0, 100.0, 0.0).into()),
-                            transform: tr,
-                            ..Default::default()
-                        });
-                });
-        });
+        home.get_faces(resource.spaces)
+            .into_iter()
+            .for_each(|direction| {
+                let tr = match direction {
+                    Direction3D::XNega => Transform {
+                        translation: center + Vec3::new(-face_half, 0.0, 0.0),
+                        rotation: Quat::from_rotation_z(90_f32.to_radians()),
+                        ..Default::default()
+                    },
+                    Direction3D::XPosi => Transform {
+                        translation: center + Vec3::new(face_half, 0.0, 0.0),
+                        rotation: Quat::from_rotation_z(-90_f32.to_radians()),
+                        ..Default::default()
+                    },
+                    Direction3D::YNega => Transform {
+                        translation: center + Vec3::new(0.0, -face_half, 0.0),
+                        rotation: Quat::from_rotation_z(180_f32.to_radians()),
+                        ..Default::default()
+                    },
+                    Direction3D::YPosi => Transform {
+                        translation: center + Vec3::new(0.0, face_half, 0.0),
+                        ..Default::default()
+                    },
+                    Direction3D::ZNega => Transform {
+                        translation: center + Vec3::new(0.0, 0.0, -face_half),
+                        rotation: Quat::from_rotation_x(-90_f32.to_radians()),
+                        ..Default::default()
+                    },
+                    Direction3D::ZPosi => Transform {
+                        translation: center + Vec3::new(0.0, 0.0, face_half),
+                        rotation: Quat::from_rotation_x(90_f32.to_radians()),
+                        ..Default::default()
+                    },
+                };
+
+                commands
+                    .spawn()
+                    .insert(CubeHome(home))
+                    .insert(CubePos(home))
+                    .insert(CubeFace(direction))
+                    .insert_bundle(PbrBundle {
+                        mesh: meshes.add(Mesh::from(shape::Plane { size: face_size })),
+                        material: materials.add(Color::rgb(1.0, 0.5, 0.0).into()),
+                        transform: tr,
+                        ..Default::default()
+                    });
+            });
+    });
 }
